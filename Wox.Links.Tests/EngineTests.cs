44b2261d9acce188c28bb99e19fab7e91ec27856
Artiom <akuciuk@greentube.com>
2018-11-02 09:43:33 +0100

    Save link functionality

---
+++ Wox.Links.Tests/EngineTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NSubstitute;
+using Wox.Plugin;
+using Xunit;
+
+namespace Wox.Links.Tests
+{
+    public class EngineTests
+    {
+        private Engine _engine;
+
+        private IParser _parser;
+
+        private Query _query = new Query
+        {
+            Terms = new[] {"-save", "https://jira.com", "jj"}
+        };
+
+        public EngineTests()
+        {
+            _parser = Substitute.For<IParser>();
+            _engine = new Engine(new[] {_parser});
+        }
+
+        [Fact]
+        public void NoParserFound_ReturnEmptyResult()
+        {
+            _engine.Execute(_query).Should().BeEmpty();
+        }
+        [Fact]
+        public void ReturnResultFromParser()
+        {
+            var expectedResult = new List<Result>();
+
+            _parser.TryParse(_query.Terms, out List<Result> any)
+                .Returns(x =>
+                {
+                    x[1] = expectedResult;
+                    return true;
+                });
+            var actualResults = _engine.Execute(_query);
+
+            actualResults.Should().BeSameAs(expectedResult);
+        }
+    }
+
+    public class SaveParserTests
+    {
+        private IStorage _storage;
+
+        public SaveParserTests()
+        {
+            _storage = Substitute.For<IStorage>();
+        }
+        [Fact]
+        public void SaveTerms_ReturnTrueAndProposeToSave()
+        {
+            var saveParser = new SaveParser(_storage);
+
+            saveParser.TryParse(new[] {"--save", "https://some.com/link", "Shortcut"}, out List<Result> results).Should().BeTrue();
+            results.Should().HaveCount(1);
+
+            var result = results[0];
+            result.Title.Should().Be("Save the link as 'Shortcut'");
+            result.SubTitle.Should().Be("https://some.com/link");
+
+            result.Action(new ActionContext());
+            _storage.Received(1).Add("Shortcut", "https://some.com/link");
+        }
+    }
+}
--- Wox.Links/Class1.cs
+++ Wox.Links/Class1.cs
@@ -1,6 +1,131 @@
-﻿namespace Wox.Links
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using Autofac;
+using Wox.Plugin;
+
+namespace Wox.Links
 {
-  public class Class1
-  {
-  }
+    internal class Main : IPlugin
+    {
+        private IEngine _engine;
+
+        public List<Result> Query(Query query)
+        {
+            var result = _engine.Execute(query);
+            return result.ToList();
+        }
+
+        public void Init(PluginInitContext context)
+        {
+            Startup.Initialize(context);
+            _engine = Startup.Resolve<IEngine>();
+        }
+    }
+
+    public interface IStorage
+    {
+        void Add(string shortcut, string url);
+    }
+
+
+    internal interface IEngine
+    {
+        List<Result> Execute(Query query);
+    }
+
+    public class Startup
+    {
+        private static IContainer _container;
+
+        public static void Initialize(PluginInitContext pluginInitContext)
+        {
+            var container = new ContainerBuilder();
+            container.RegisterInstance(pluginInitContext).AsSelf();
+            container.RegisterType<Engine>().AsImplementedInterfaces().SingleInstance();
+            container.RegisterType<SettingsProvider>().AsImplementedInterfaces().SingleInstance();
+            _container = container.Build();
+        }
+
+        public static T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+    }
+
+    public class SaveParser : IParser
+    {
+        static Regex SaveMatch = new Regex(@"--save\b|-s\b", RegexOptions.IgnoreCase);
+
+        static Regex LinkMatch =
+            new Regex(@"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$");
+
+        private IStorage _storage;
+
+        public SaveParser(IStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public bool TryParse(string[] terms, out List<Result> results)
+        {
+            results = new List<Result>();
+            if (terms.Length < 3)
+                return false;
+
+            var saveKeyWord = terms.FirstOrDefault(x => SaveMatch.IsMatch(x));
+            var linkWord = terms.FirstOrDefault(x => LinkMatch.IsMatch(x));
+
+            if (saveKeyWord == null || linkWord == null)
+                return false;
+
+            var shortCut = terms.First(x => x != saveKeyWord && x != linkWord);
+            results.Add(CreateResult(shortCut, linkWord));
+            return true;
+        }
+
+        private Result CreateResult(string shortCut, string linkWord)
+        {
+            return new Result
+            {
+                Title = $"Save the link as \'{shortCut}\'",
+                SubTitle = linkWord,
+                Action = context =>
+                {
+                    _storage.Add(shortCut, linkWord);
+                    return true;
+                }
+            };
+        }
+    }
+
+    public interface IParser
+    {
+        bool TryParse(string[] terms, out List<Result> results);
+    }
+
+    public class Engine : IEngine
+    {
+        private IEnumerable<IParser> _parsers;
+
+        public Engine(IEnumerable<IParser> parsers)
+        {
+            _parsers = parsers;
+        }
+
+        public List<Result> Execute(Query query)
+        {
+            var terms = query.Terms;
+            foreach (var parser in _parsers)
+            {
+                if (parser.TryParse(terms, out List<Result> results))
+                    return results;
+            }
+
+            return new List<Result>();
+        }
+    }
 }
